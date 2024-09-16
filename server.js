9acbf2cb1dbc1d4541d483f83bd598ffd77dd884
QuocTrinh-coder <harrytrinh2004@gmail.com>
2024-09-16 02:46:19 -0700

    mobile css update

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = 8080;
 
 app.set('trust proxy', 1); // Add this line to trust the first proxy
 
-let randomNumbers = [ 23, 8, 0, 29, 0 ];
+let randomNumbers = [ 40, 52, 36, 58, 0 ];
 let memberData = {}; // Variable to store the fetched data
 
 
